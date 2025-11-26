0473b544b3c8e6d42d1c512840f94237172f6099
Vince Broz <vince.broz@calendly.com>
2025-11-26 15:42:19 -0500

    Try log

--- lib.sh
+++ lib.sh
@@ -108,16 +108,16 @@ function get_normalized_package_list {
   local packages=$(echo "${1}" \
     | sed 's/[,\]/ /g; s/\s\+/ /g; s/^\s\+//g; s/\s\+$//g' \
     | sort -t' ')
-  >&2 echo "packages is now '${packages}'"
+  log "packages is now '${packages}'"
   local script_dir="$(dirname -- "$(realpath -- "${0}")")"
-  >&2 echo "script_dir is '${sript_dir}'"
+  log "script_dir is '${sript_dir}'"
 
   local architecture=$(dpkg --print-architecture)
-  >&2 echo "architecture is '${architecture}'"
+  log "architecture is '${architecture}'"
   if [ "${architecture}" == "arm64" ]; then
     ${script_dir}/apt_query-arm64 normalized-list ${packages}
   else
-    >&2 echo "Running ${script_dir}/apt_query-x86 normalized-list ${packages}"
+    log "Running ${script_dir}/apt_query-x86 normalized-list ${packages}"
     ${script_dir}/apt_query-x86 normalized-list ${packages}
   fi
 }
